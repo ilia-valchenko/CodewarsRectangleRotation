eec487f0263a0a358162ed61fe31c7de5303d560
Ilya Valchanka <ilia.valchenko.epam@gmail.com>
2017-08-01 16:25:41 +0300

    Add VS project.

---
+++ RectangleRotation/RectangleRotantion/Kata.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace myjinxin
+{
+    public class Kata
+    {
+        public int rectangleRotation(int a, int b)
+        {
+            if (a < 2 || a > 10000)
+            {
+                throw new ArgumentException($"The {nameof(a)} must be greater than 2 and less than 10000.");
+            }
+
+            if (a % 2 != 0)
+            {
+                throw new ArgumentException($"The {a} must be an even number.");
+            }
+
+            if (b < 2 || b > 10000)
+            {
+                throw new ArgumentException($"The {nameof(b)} must be greater than 2 and less than 10000.");
+            }
+
+            if (b % 2 != 0)
+            {
+                throw new ArgumentException($"The {b} must be an even number.");
+            }
+
+            //----------------------
+            //int numberOfColumns = (int) Math.Floor((double) a / Math.Sqrt(2)) + 1;
+            // ---------------------
+            //int numberOfColumns = (int)(a / Math.Sqrt(2)) + 1;
+            //--------------------------
+
+            int numberOfColumns = (int)(a / (Math.Sqrt(2) / 2));
+
+            //int columnHeight = (int)Math.Floor((double)b / Math.Sqrt(2)) + 1;
+
+            int columnHeight = (int)(b / Math.Sqrt(2));
+
+            int points = 0;
+
+            Console.WriteLine($"ORIGINAL Number of columns: {numberOfColumns}");
+            numberOfColumns++;
+            Console.WriteLine($"number of columns: {numberOfColumns}");
+
+            Console.WriteLine($"ORIGINAL Column height: {columnHeight}");
+            columnHeight++;
+            Console.WriteLine($"column height: {columnHeight}");
+
+            for (int i = 0; i < numberOfColumns; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    points += columnHeight;
+                }
+                else
+                {
+                    points += columnHeight - 1;
+                }
+            }
+
+            //if (numberOfColumns % 2 != 0)
+            //{
+            //    points--;
+            //}
+
+            return points;
+        }
+    }
+}
---
+++ RectangleRotation/RectangleRotantion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using myjinxin;
+
+namespace RectangleRotantion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata kata = new Kata();
+
+            int[,] arr = 
+            {
+                { 6, 4, 23 },
+                { 30, 2, 65 },
+                { 8, 6, 49 },
+                { 16, 20, 333 },
+                { 8844, 8228, 72771979 },
+                { 9756, 6618, 64563061 },
+                { 4348, 9092, 39534995 }
+            };
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                int a = arr[i, 0];
+                int b = arr[i, 1];
+                int expectedResult = arr[i, 2];
+
+                Console.WriteLine($"[{a},{b}] --> {kata.rectangleRotation(a ,b)} --> expected result: {expectedResult}\n");
+            }
+
+            Console.WriteLine("\n\nTap to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
